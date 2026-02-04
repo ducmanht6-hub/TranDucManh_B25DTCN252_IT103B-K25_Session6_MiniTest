36dc267ab6f4df7c99083ef4ce8947e5623e1aef
ducmanht6-hub <ducmanht6@gmail.com>
2026-02-04 07:28:36 +0700

    Add files via upload

---
+++ Session 06.2.js
@@ -0,0 +1,11 @@
+let price = ["100", "200", "300", "400"];
+let count = 0
+for (let i = 0; i < price; i++) {
+    console.log(price);
+    count++
+}
+for (let j = 0; ) {
+    if (!Object.hasOwn(object, key)) continue;
+    
+    const element = object[key];   
+}

306f7623a94ffadd4a9369e46397386db93b052a
ducmanht6-hub <ducmanht6@gmail.com>
2026-02-04 07:46:34 +0700

    Delete Session 06.1.js

--- Session 06.1.js
+++
@@ -1,9 +0,0 @@
-let students = ["Quý", "Nam", "Lan", "Hùng", "Nam"];
-let newStudents = students.reverse; //Không làm thay đổi giá trị mảng gốc
-console.log(newStudents);
-let check = students.indexOf["Lan"];
-if(students !== -1){
-    console.log("Tên Lan tồn tại trong mảng"); 
-}else{
-    console.log("Tên Lan không tồn tại trong mảng");
-}
